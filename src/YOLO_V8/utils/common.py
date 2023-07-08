2dac74de33a2fd7808cd70288e4e362f65d71c93
g0urav-hustler <gouravchouhan966@gmail.com>
2023-07-08 21:55:24 +0530

    update common file

--- src/YOLO_V8/utils/common.py
+++ src/YOLO_V8/utils/common.py
@@ -8,6 +8,7 @@ import yaml
 import os
 import json
 import joblib
+import shutil
 
 
 
@@ -34,16 +35,17 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 
 @ensure_annotations
-def create_directories(path_to_directories: list, verbose=True):
+def create_directories(path_to_directories: list):
     """create list of directories
     Args:
         path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
         if not os.path.isdir(path):
             os.makedirs(path, exist_ok=True)
             logger.info(f"created directory at: {path}")
+        else:
+            logger.info(f"already created directory: {path}")
 
 @ensure_annotations
 def join_path(current_path: str, add_folder: str) :
@@ -57,6 +59,33 @@ def join_path(current_path: str, add_folder: str) :
     ResultPath = os.path.join(current_path, add_folder)
 
     return ResultPath
+
+@ensure_annotations
+def copy_files( files_names : list, source_dir :str, target_dir: str, file_extension: str):
+    """Copy files form source dir to target dir
+    Args:
+        file_names (list): list of files
+        source_dir (str): source directory path
+        target_dr (str): target directory path
+        file_extension (str): particular extension of file
+    """
+    file_count = 0
+    for files in files_names:
+            
+        if file_extension:
+            files = files + file_extension
+
+        file_source_path = join_path(source_dir, files)
+        file_target_path = join_path(target_dir, files)
+
+        if not os.path.isfile(file_target_path):
+            shutil.copy(file_source_path, file_target_path)
+            file_count = file_count + 1
+        
+        if file_count:
+            logger.info(f"{file_count} Files copied from {source_dir} to {target_dir}.")
+        else:
+            logger.inf(f"All files are already in {target_dir}.")
 
 @ensure_annotations
 def save_json(path: Path, data: dict):

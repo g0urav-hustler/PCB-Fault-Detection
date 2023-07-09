6435746f4d62f5f8c4429639ff42d9e2f7f5c131
g0urav-hustler <gouravchouhan966@gmail.com>
2023-07-09 13:52:55 +0530

    add data processing pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.YOLO_V8 import logger
 from src.YOLO_V8.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.YOLO_V8.pipeline.stage_02_data_processing import DataProcessingTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -11,3 +12,18 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+######
+
+STAGE_NAME = "Data Processing Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataProcessingTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
